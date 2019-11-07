3a79cebed821aa160bd05c05176c6481f29cbc9a
Danny Hadley <dadleyy@gmail.com>
2019-11-07 00:57:31 -0500

    add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,33 @@
+//! This crate provides a lightweight and potentially incomplete http head parser implementation
+//! for async-std readers.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use std::boxed::Box;
+//! use std::error::Error;
+//!
+//! use elaine::recognize;
+//! use async_std::task::block_on;
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!   block_on(async {
+//!     let mut req: &[u8] = b"GET /elaine HTTP/1.1\r\nContent-Length: 3\r\n\r\nhey";
+//!     let result = recognize(&mut req).await.unwrap();
+//!     assert_eq!(result.method(), Some("GET".to_string()));
+//!     assert_eq!(result.len(), Some(3));
+//!     assert_eq!(std::str::from_utf8(req), Ok("hey"));
+//!   });
+//!   Ok(())
+//! }
+//! ```
+//!
+//! [blink(1)]: https://blink1.thingm.com
 extern crate async_std;
 
 use async_std::io::Read;
 use async_std::prelude::*;
 use std::io::{Error, ErrorKind};
-
-#[derive(PartialEq, Clone, Default, Debug)]
-struct Cursor {
-  first: Option<()>,
-  second: Option<()>,
-  count: u8,
-  current: u8,
-}
 
 #[derive(Debug, Default)]
 pub struct RequestLine {
