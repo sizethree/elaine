acc2aa9a7bb0c8e2ddb994fd421cb531517ec1cf
Danny Hadley <dadleyy@gmail.com>
2019-11-08 16:49:08 -0500

    0.2.0 - standards + more (#4)
    * 0.2.0 - request method enumeration
    
    * version matching, bugfix
    
    * update readme
    
    * add readme
    
    * moving things around
    
    * example that echos
    
    * custom stack type
    
    * rm boxes

--- README.md
+++ README.md
@@ -5,27 +5,43 @@
 This crate provides a lightweight and potentially incomplete http head parser implementation
 for async-std readers.
 
+## Goals &amp; Stuff
+
+This crate is intended to provide an HTTP head parser for async [`readers`] with a focus on simplicity and safety;
+while performance is appreciated, safety and simplicity take priority.
+  
+### On Safety
+
+The api provided by this crate will _never_ include `unsafe` code directly, including code that would otherwise
+improve the performance of the libary. In addition, the main export - [`recognize`][recognize] - provides the
+guaruntee that it will _never_ over-read bytes from a reader, again at the potential loss of performance.
+
+### On Simplictity
+
+This crate does not include the [`http`][http-crate] in it's dependencies; though well-maintained and useful as it is,
+it would introduce a super set of functionality that is not required for this implementation. This decision is not 
+in any way meant to discourage other developers from using that library.
+
 ## Example
 
 ```rust
 use std::boxed::Box;
 use std::error::Error;
 
-use elaine::recognize;
+use elaine::{recognize, RequestMethod};
 use async_std::task::block_on;
 
 fn main() -> Result<(), Box<dyn Error>> {
   block_on(async {
     let mut req: &[u8] = b"GET /elaine HTTP/1.1\r\nContent-Length: 3\r\n\r\nhey";
     let result = recognize(&mut req).await.unwrap();
-    assert_eq!(result.method(), Some("GET".to_string()));
+    assert_eq!(result.method(), Some(RequestMethod::GET));
     assert_eq!(result.len(), Some(3));
     assert_eq!(std::str::from_utf8(req), Ok("hey"));
   });
   Ok(())
 }
 ```
-
 
 | elaine |
 | --- |
@@ -45,3 +61,6 @@ See [CONTRIBUTING](/CONTRIBUTING.md).
 [crates.url]: https://crates.io/crates/elaine
 [crates.img]: https://img.shields.io/crates/v/elaine
 [elaine]: https://user-images.githubusercontent.com/1545348/68368941-1cee4e80-0107-11ea-8e87-47cb29cf8e15.gif
+[`readers`]: https://docs.rs/async-std/0.99.12/async_std/io/trait.Read.html
+[http-crate]: https://crates.io/crates/http
+[recognize]: https://docs.rs/elaine/0.1.1/elaine/fn.recognize.html
--- tests/recognize.rs
+++ tests/recognize.rs
@@ -2,66 +2,88 @@
 
 mod helpers;
 
+use async_std::prelude::*;
 use async_std::task::block_on;
-use elaine::recognize;
+use elaine::{recognize, RequestMethod};
 use helpers::AsyncBuffer;
 
 fn buffer_from(source: &[u8]) -> AsyncBuffer {
   AsyncBuffer::new(format!(
-    "GET /first HTTP1.1\r\nComplex: {}",
+    "GET /first HTTP/1.1\r\nComplex: {}",
     std::str::from_utf8(source).unwrap()
   ))
+}
+
+#[test]
+fn test_invalid_header_line() {
+  let mut buff = AsyncBuffer::new("FOOBAR\r\nHost: 0.0.0.0:8080\r\n\r\n");
+  let result = block_on(async { recognize(&mut buff).await });
+  assert!(result.is_err());
+}
+
+#[test]
+fn test_invalid_method() {
+  let mut buff = AsyncBuffer::new("FOOBAR /hello HTTP/1.1\r\nHost: 0.0.0.0:8080\r\n\r\n");
+  let result = block_on(async { recognize(&mut buff).await });
+  assert!(result.is_err());
+}
+
+#[test]
+fn test_invalid_version() {
+  let mut buff = AsyncBuffer::new("FOOBAR /hello GARBAGE\r\nHost: 0.0.0.0:8080\r\n\r\n");
+  let result = block_on(async { recognize(&mut buff).await });
+  assert!(result.is_err());
 }
 
 #[test]
 fn test_with_auth() {
   let mut buff = AsyncBuffer::new("POST / HTTP/1.1\r\nHost: 0.0.0.0:8080\r\n\r\n");
   let result = block_on(async { recognize(&mut buff).await });
-  println!("result: {:?}", result);
+  assert!(result.is_ok());
 }
 
 #[test]
-fn test_rere_two() {
+fn test_recognize_two() {
   let tail: &[u8] = &[0x61, 0x61, 0x61, 0xC9, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
-  let mut full = AsyncBuffer::new(format!("GET /first HTTP1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
+  let mut full = AsyncBuffer::new(format!("GET /first HTTP/1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
   let result = block_on(async { recognize(&mut full).await });
   assert!(result.is_ok())
 }
 
 #[test]
-fn test_rere_three() {
+fn test_recognize_three() {
   let tail: &[u8] = &[0x61, 0x61, 0xE0, 0xA1, 0x98, 0x0d, 0x0a, 0x0d, 0x0a];
-  let mut full = AsyncBuffer::new(format!("GET /first HTTP1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
+  let mut full = AsyncBuffer::new(format!("GET /first HTTP/1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
   let result = block_on(async { recognize(&mut full).await });
   assert!(result.is_ok())
 }
 
 #[test]
-fn test_rere_full() {
+fn test_recognize_full() {
   let tail: &[u8] = &[0xF0, 0x90, 0x86, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
-  let mut full = AsyncBuffer::new(format!("GET /first HTTP1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
+  let mut full = AsyncBuffer::new(format!("GET /first HTTP/1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
   let result = block_on(async { recognize(&mut full).await });
   assert!(result.is_ok())
 }
 
 #[test]
-fn test_rere_four() {
+fn test_recognize_four() {
   let tail: &[u8] = &[0x61, 0xF0, 0x90, 0x86, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
-  let mut full = AsyncBuffer::new(format!("GET /first HTTP1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
+  let mut full = AsyncBuffer::new(format!("GET /first HTTP/1.1\r\n{}", std::str::from_utf8(tail).unwrap()));
   let result = block_on(async { recognize(&mut full).await });
   assert!(result.is_ok())
 }
 
 #[test]
-fn test_rere_after() {
-  let mut full = AsyncBuffer::new("GET /foo HTTP1.1\r\nHost: 8080\r\nContent-Length: 3\r\n\r\nhey");
+fn test_recognize_after() {
+  let mut full = AsyncBuffer::new("GET /foo HTTP/1.1\r\nHost: 8080\r\nContent-Length: 3\r\n\r\nhey");
   let result = block_on(async { recognize(&mut full).await });
   assert!(result.is_ok());
   assert_eq!(format!("{}", full), format!("{}", AsyncBuffer::new("hey")));
 }
 
 #[test]
-fn test_recog_utf8_boundary_dangle_two() {
+fn test_recognize_utf8_boundary_dangle_two() {
   let buf: &[u8] = &[0x61, 0x61, 0x61, 0xE0, 0xA1, 0x98, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -70,7 +92,7 @@ fn test_recog_utf8_boundary_dangle_two() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_dangle_three() {
+fn test_recognize_utf8_boundary_dangle_three() {
   let buf: &[u8] = &[0x61, 0x61, 0x61, 0xF0, 0x90, 0x86, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -78,7 +100,7 @@ fn test_recog_utf8_boundary_dangle_three() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_half_debt_one() {
+fn test_recognize_utf8_boundary_half_debt_one() {
   let buf: &[u8] = &[0x61, 0x61, 0xC9, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -86,7 +108,7 @@ fn test_recog_utf8_boundary_half_debt_one() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_half_debt_two() {
+fn test_recognize_utf8_boundary_half_debt_two() {
   let buf: &[u8] = &[0x61, 0xC9, 0x92, 0x61, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -94,7 +116,7 @@ fn test_recog_utf8_boundary_half_debt_two() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_half_dangle_one() {
+fn test_recognize_utf8_boundary_half_dangle_one() {
   let buf: &[u8] = &[0x61, 0x61, 0xE0, 0xA1, 0x98, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -102,7 +124,7 @@ fn test_recog_utf8_boundary_half_dangle_one() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_half_dangle_three() {
+fn test_recognize_utf8_boundary_half_dangle_three() {
   let buf: &[u8] = &[0x61, 0x61, 0xF0, 0x90, 0x86, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -110,7 +132,7 @@ fn test_recog_utf8_boundary_half_dangle_three() {
 }
 
 #[test]
-fn test_recog_utf8_boundary_half_debt_one_four() {
+fn test_recognize_utf8_boundary_half_debt_one_four() {
   let buf: &[u8] = &[0x61, 0xF0, 0x90, 0x86, 0x92, 0x0d, 0x0a, 0x0d, 0x0a];
   let mut full = buffer_from(buf);
   let result = block_on(async { recognize(&mut full).await });
@@ -126,13 +148,13 @@ fn test_single_char_utf8() {
 }
 
 #[test]
-fn test_recog_http_example() {
+fn test_recognize_http_example() {
   let mut buffer = AsyncBuffer::new(format!(
     "{}{}{}{}{}{}{}{}{}{}{}{}",
     "GET ", "/hel", "lo-w", "orld", " HTT", "P/1.", "1\r\nC", "onte", "nt-L", "engt", "h: 3", "\r\n\r\n"
   ));
   let result = block_on(async { recognize(&mut buffer).await });
-  assert_eq!(result.unwrap().method(), Some("GET".to_string()));
+  assert_eq!(result.unwrap().method(), Some(RequestMethod::GET));
 }
 
 #[test]
@@ -141,7 +163,7 @@ fn recognize_valid_without_body() {
   let result = block_on(async { recognize(&mut buffer).await });
   assert!(result.is_ok());
   let head = result.unwrap();
-  assert_eq!(head.method(), Some(String::from("GET")));
+  assert_eq!(head.method(), Some(RequestMethod::GET));
   assert_eq!(head.len(), None);
 }
 
@@ -151,7 +173,7 @@ fn recognize_valid_with_len() {
   let result = block_on(async { recognize(&mut buffer).await });
   assert!(result.is_ok());
   let head = result.unwrap();
-  assert_eq!(head.method(), Some(String::from("GET")));
+  assert_eq!(head.method(), Some(RequestMethod::GET));
   assert_eq!(head.len(), Some(10));
 }
 
@@ -167,4 +189,15 @@ fn recognize_fail_bad_start() {
   let mut buffer = AsyncBuffer::new("\r\n");
   let result = block_on(async { recognize(&mut buffer).await });
   assert!(result.is_err());
+}
+
+#[test]
+fn recognize_and_read_after() {
+  let mut buffer = AsyncBuffer::new("POST /create HTTP/1.1\r\nContent-Length: 3\r\n\r\nhey");
+  let result = block_on(async { recognize(&mut buffer).await });
+  assert!(result.is_ok());
+  let mut rem: Vec<u8> = vec![0x00, 0x00, 0x00];
+  let result = block_on(async { buffer.read(&mut rem).await });
+  assert_eq!(result.unwrap(), 3);
+  assert_eq!(String::from_utf8(rem).unwrap(), "hey");
 }
